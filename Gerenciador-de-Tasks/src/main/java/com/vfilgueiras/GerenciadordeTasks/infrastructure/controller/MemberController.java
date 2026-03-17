bb2eee7f632d0e262dcf76e6ec63c4cda1a5b82b
v-filgueiras <vfilgueiras.dev@gmail.com>
2026-03-17 11:14:26 -0400

    adicionado ao controller funcao para criar membro

---
+++ Gerenciador-de-Tasks/src/main/java/com/vfilgueiras/GerenciadordeTasks/infrastructure/controller/MemberController.java
@@ -0,0 +1,28 @@
+package com.vfilgueiras.GerenciadordeTasks.infrastructure.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vfilgueiras.GerenciadordeTasks.domain.entity.MemberEntity;
+import com.vfilgueiras.GerenciadordeTasks.domain.service.MemberService;
+import com.vfilgueiras.GerenciadordeTasks.infrastructure.dto.MemberDataDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<MemberEntity> createMember(@RequestBody MemberDataDTO dataDTO) {
+        MemberEntity newMember = memberService.createMember(dataDTO);
+        return ResponseEntity.status(201).body(newMember);
+    }
+}
