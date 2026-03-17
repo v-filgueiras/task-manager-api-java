16698a5d432b88116bde2fae8f5c169135ed05f1
v-filgueiras <vfilgueiras.dev@gmail.com>
2026-03-17 10:55:26 -0400

    adicionando metodo de criacao de membros

---
+++ Gerenciador-de-Tasks/src/main/java/com/vfilgueiras/GerenciadordeTasks/domain/service/MemberService.java
@@ -0,0 +1,29 @@
+package com.vfilgueiras.GerenciadordeTasks.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import com.vfilgueiras.GerenciadordeTasks.domain.entity.MemberEntity;
+import com.vfilgueiras.GerenciadordeTasks.domain.repository.MemberRepository;
+import com.vfilgueiras.GerenciadordeTasks.infrastructure.dto.MemberDataDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    
+    public MemberEntity createMember(MemberDataDTO memberDTO) {
+        MemberEntity newMember = MemberEntity
+        .builder()
+        .secret(memberDTO.getSecret())
+        .name(memberDTO.getName())
+        .email(memberDTO.getEmail())
+        .build();
+
+        memberRepository.save(newMember);
+
+        return newMember;
+    }
+}
