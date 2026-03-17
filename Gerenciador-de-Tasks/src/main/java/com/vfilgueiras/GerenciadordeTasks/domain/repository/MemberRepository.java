bac121d243e4be53203e1bbe1193575fd518b7f1
v-filgueiras <vfilgueiras.dev@gmail.com>
2026-03-17 11:15:17 -0400

    adicionando annotation

--- Gerenciador-de-Tasks/src/main/java/com/vfilgueiras/GerenciadordeTasks/domain/repository/MemberRepository.java
+++ Gerenciador-de-Tasks/src/main/java/com/vfilgueiras/GerenciadordeTasks/domain/repository/MemberRepository.java
@@ -1,6 +1,9 @@
 package com.vfilgueiras.GerenciadordeTasks.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.vfilgueiras.GerenciadordeTasks.domain.entity.MemberEntity;
 
+@Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, String>{}
