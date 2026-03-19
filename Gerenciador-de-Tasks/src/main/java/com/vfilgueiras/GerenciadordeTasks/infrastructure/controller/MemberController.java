88807b2fa548a773475cc91d9ef2ff6a7d31a88b
v-filgueiras <vfilgueiras.dev@gmail.com>
2026-03-19 14:52:47 -0400

    atualizando controller com funcao de get dos membros

--- Gerenciador-de-Tasks/src/main/java/com/vfilgueiras/GerenciadordeTasks/infrastructure/controller/MemberController.java
+++ Gerenciador-de-Tasks/src/main/java/com/vfilgueiras/GerenciadordeTasks/infrastructure/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.vfilgueiras.GerenciadordeTasks.infrastructure.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +26,11 @@ public class MemberController {
     public ResponseEntity<MemberEntity> createMember(@RequestBody MemberDataDTO dataDTO) {
         MemberEntity newMember = memberService.createMember(dataDTO);
         return ResponseEntity.status(201).body(newMember);
+    }
+
+    @GetMapping("/{memberID}")
+    public ResponseEntity<MemberEntity> getMemberByID(@PathVariable String memberID) {
+        MemberEntity dataMember = memberService.getMemberByID(memberID);
+        return ResponseEntity.ok(dataMember);
     }
 }
